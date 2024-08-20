97601d2f739c02289233e25d86f8226d5e676b3c
Raymon01 <ramonberke01@gmail.com>
2024-08-20 04:45:51 +0300

    chore: Update service configurations for customer and order services in main.py

--- app/main.py
+++ app/main.py
@@ -21,14 +21,14 @@ async def receive_payload():
         "customer-service": {
             "name": "customer-service",
             "image": "ghcr.io/tesodev-order-application/microservices-customer-service",
-            "ports": {"8080" : "8001"},
+            "ports": {"8001" : "8001"},
             "detach": True,
             "restart_policy": {"Name": "always"}
         },
         "order-service": {
             "name": "order-service",
             "image": "ghcr.io/tesodev-order-application/microservices-order-service",
-            "ports": {"8080" : "8002"},
+            "ports": {"8002" : "8002"},
             "detach": True,
             "restart_policy": {"Name": "always"}
         },
